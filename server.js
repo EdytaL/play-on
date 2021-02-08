71cbd8b72cfa203110939d38b30f18af2f179a0b
EdytaLy <lysiakedyta@gmail.com>
2021-02-08 18:04:20 +0100

    specify app server for heroku

---
+++ server.js
@@ -0,0 +1,17 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files from the dist directory
+app.use(express.static(__dirname + '/dist/play-on'));
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/play-on/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8099);
+console.log('App running');
